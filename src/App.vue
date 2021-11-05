ec8693633730d69f3f0ee2db0727e245b265292c
Tiffany <tiffanyphilippi15@gmail.com>
2021-11-05 09:25:33 -0300

    photo list + style + panel shared

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,56 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+  <div class="body">
+    <h1 class="title text-center">{{ title }}</h1>
+    <ul class="photos-list">
+      <li v-for="photo of photos" class="item-list">
+        <app-card-panel :title="photo.titulo">
+          <img :src="photo.url" :alt="photo.titulo" class="photo" />
+        </app-card-panel>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Panel from './components/shared/panel/Panel.vue';
 export default {
-  name: 'app',
-  data () {
+  components: {
+    'app-card-panel': Panel
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      title: "Alurapic",
+      photos: []
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(
+        photos => (this.photos = photos),
+        err => console.log(err)
+      );
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.body {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+
+  .photos-list {
+    display: flex;
+    list-style: none;
+    flex-wrap: wrap;
+    .item-list {
+      margin: 0 10px 40px;
+    }
+  }
+}
+.text-center {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
---
+++ src/components/shared/panel/Panel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="panel text-center">
+    <h2 class="photo-title">{{ title }}</h2>
+    <slot class="content-panel"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+};
+</script>
+
+<style lang="scss">
+.panel {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #d4ddeb;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: auto;
+  vertical-align: top;
+  .photo-title {
+    background-color: #fdf054;
+    margin: 0;
+    text-transform: uppercase;
+    padding: 10px;
+  }
+}
+</style>
